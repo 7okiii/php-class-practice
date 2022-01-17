9a296a62b15927fef7a83cb68d270d30fdc914c4
Nnookk <naokiogata@NaokinoAir.com>
2022-01-17 14:16:01 -0500

    be able to save data to the file

--- class.php
+++ class.php
@@ -29,15 +29,18 @@
         <input type="text" name="lname">
         <label for="email">Email Address</label>
         <input type="email" name="email">
+        <label for="course">Course</label>
+        <input type="text" name="course">
         <button type="submit">Save</button>
     </form>
 <?php
     class User {
-        function __construct($fname,$lname,$email)
+        function __construct($fname,$lname,$email,$course)
         {
             $this->fname = $fname;
             $this->lname = $lname;
             $this->email = $email;
+            $this->course = $course;
         }
         // set method
         function set_fname($fname) {
@@ -49,6 +52,9 @@
         function set_email($email) {
             $this->email = $email;
         }
+        function set_course($course) {
+            $this->course = $course;
+        }
         // get method
         function get_fname() {
             return $this->fname;
@@ -59,11 +65,28 @@
         function get_email() {
             return $this->email;
         }
+        function get_course() {
+            return $this->course;
+        }
+        // function to save the user data to the directory
+        function data_save($destFile) {
+            $file_name = $this->fname.'_'.$this->lname.'.txt';
+            $file = fopen($destFile.'/'.$file_name,'w');
+            fwrite($file,'First Name: '.$this->fname."\n");
+            fwrite($file,'Last Name: '.$this->lname."\n");
+            fclose($file);
+        }
     }
-    $user = new User($_POST['fname'],$_POST['lname'],$_POST['email']);
+    $user = new User($_POST['fname'],$_POST['lname'],$_POST['email'],$_POST['course']);
     echo $user->get_fname().'<br>';
     echo $user->get_lname().'<br>';
     echo $user->get_email().'<br>';
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $dest_file = "./course/".$_POST['course'];
+        mkdir($dest_file);
+        $user->data_save($dest_file);
+    }
 ?>    
 </body>
 </html>
